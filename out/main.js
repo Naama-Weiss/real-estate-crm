2049f8a6008f5dbd49bacdaea8fa840e8a533739
Naama-Weiss <naamoule@gmail.com>
2024-11-10 11:20:28 +0200

    Initial commit

---
+++ out/main.js
@@ -0,0 +1,6 @@
+console.log("Welcome to Real Estate CRM!");
+
+const root = document.getElementById("root");
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "This is a dynamically added paragraph.";
+root.appendChild(newParagraph);
