7f4e1b0d139e5422f7a98d96332f675813a97177
Naama-Weiss <naamoule@gmail.com>
2024-11-10 11:37:46 +0200

    Add .gitignore and remove ignored files

--- out/main.js
+++
@@ -1,6 +0,0 @@
-console.log("Welcome to Real Estate CRM!");
-
-const root = document.getElementById("root");
-const newParagraph = document.createElement("p");
-newParagraph.textContent = "This is a dynamically added paragraph.";
-root.appendChild(newParagraph);
